124971ad4560d847ba3ab7160fd791e7895b38da
Ankit152 <ankitkurmi152@gmail.com>
2023-01-18 17:58:32 +0530

    renaming the directory

--- 04Operators/arithmeticOperator/arithmeticOperator.sh
+++ 04Operators/arithmeticOperator/arithmeticOperator.sh
@@ -12,3 +12,17 @@ echo "x modulo y is: $(($x % $y))"
 echo "y modulo x is: $(($y % $x))"
 echo "x to the y is: $(($x ** $y))"
 echo "y to the power x is: $(($y ** $x))"
+
+((++x))
+echo "Pre-increment of x: $x"
+((x++))
+echo "Post-increment of x: $x"
+((y--))
+echo "Post-decrement of y: $y"
+((--y))
+echo "Pre-decrement of y: $y"
+
+((x += y))
+echo "x = $x and y = $y"
+((y -= x))
+echo "x = $x and y = $y"
