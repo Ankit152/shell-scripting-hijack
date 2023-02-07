5c6417c26d32665daa7ac55b998d41b5c39a5733
Ankit152 <ankitkurmi152@gmail.com>
2023-02-07 22:22:38 +0530

    for loops in a different way and increments

--- 07loops/forLoop.sh
+++ 07loops/forLoop.sh
@@ -11,3 +11,16 @@ for i in {1..10..2}
 do
     echo $i
 done
+
+echo "for loop in a different way with increment of 1"
+read -p "Enter a number: " num
+for (( i=0; i<$num; i+=1 ))
+do
+    echo $i
+done
+
+echo "for loop in a different way with increment of 2"
+for (( i=0; i<$num; i+=2 ))
+do
+    echo $i
+done
